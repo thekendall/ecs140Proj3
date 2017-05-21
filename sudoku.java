8bbf08f51920e7ae451d555df4c75ad0576d243b
Richard Tian-Ren Gao <richardg@pc15.cs.ucdavis.edu>
2017-05-20 18:32:12 -0700

    java sudoku file

--- sudoku.java
+++ sudoku.java
@@ -6,107 +6,99 @@ public class sudoku {
 
 	public static void main(String [] args) {
 		sudoku solver = new sudoku();
-		int board[][] = {{1,2,0,0},{4,3,0,0},{0,0,4,0},{0,0,0,1}};	
-		if(solver.isValid(board)) {
-			System.out.println("valid board");
+		int board[][] = {{1,2,0,0},{3,4,0,0},{0,0,4,0},{0,0,0,1}};	
+		if(solver.solve(board)) {
+			System.out.println("Printed solution");
 		}
 		else 
-			System.out.println("not valid board");
+			System.out.println("Not valid board");
 				
 	}
+	public boolean full(int[][] board) {
+		for(int i = 0; i < 4; i++){
+			for(int j = 0; j <4; j++) {
+				if(board[i][j] == 0)
+					return false;
+			}
+		}
+		return true;
+
+	}
 	public boolean solve(int[][] board) {
+		for(int i= 0; i< 4; i++) {
+			for(int j =0; j<4; j++) {
+				if(board[i][j] != 0) {
+					continue;
+				}
+				for(int k = 1; k < 5; k++) {
+					if(isValid(board, i, j, k)) {
+						board[i][j] = k;
+						if(solve(board))
+							return true;
+						else
+							board[i][j] = 0;
+					}
+				}
+				return false;
+			}
+		}
+		for(int i = 0; i < 4; i++) {
+			for(int j = 0; j < 4; j++) {
+				System.out.print(board[i][j]);
+			}
+			System.out.println();
+		}
 		return true;
 	}
-	public boolean isValid(int[][] board) {
-		if(checkRows(board) && checkColumns(board) && checkSquares(board))
+	public boolean isValid(int[][] board, int row, int column, int value) {
+		if(checkRows(board, row, value) && checkColumns(board, column, value) && checkSquares(board, row, column, value))
 			return true;
 		else
 			return false;
 	}
-	public boolean checkRows(int[][] board) {
-		boolean one = true;
-		boolean two = true;
-		boolean three = true;
-		boolean four = true;
-		for(int i = 0; i < 4; i++) {
-			for(int j = 0; j < 4; j++) {
-				if(board[i][j] == 0)
-					continue;
-				else if(board[i][j] == 1 && one)
-					one = false;
-				else if(board[i][j] == 2 && two)
-					two = false;
-				else if(board[i][j] == 3 && three)
-					three = false;
-				else if(board[i][j] == 4 && four)
-					four = false;
-				else
-					return false;
-			}
-			one = true;
-			two = true;
-			three = true;
-			four = true;
+	public boolean checkRows(int[][] board, int row, int value) {
+		for(int j = 0; j < 4; j++) {
+			if(board[row][j] == value)
+				return false;
 		}
 		return true;
 	}
-	public boolean checkColumns(int[][] board) {
-		boolean one = true;
-		boolean two = true;
-		boolean three = true;
-		boolean four = true;
+	public boolean checkColumns(int[][] board, int column, int value) {
 		for(int i = 0; i < 4; i++) {
-			for(int j = 0; j < 4; j++) {
-				if(board[j][i] == 0)
-					continue;
-				else if(board[j][i] == 1 && one)
-					one = false;
-				else if(board[j][i] == 2 && two)
-					two = false;
-				else if(board[j][i] == 3 && three)
-					three = false;
-				else if(board[j][i] == 4 && four)
-					four = false;
-				else
-					return false;
-			}
-			one = true;
-			two = true;
-			three = true;
-			four = true;
+			if(board[i][column] == value)
+				return false;
 		}
 		return true;
 	}
 
 	
-	public boolean checkBox(int[][] board, int x, int y) {
-		boolean one = true;
-		boolean two = true;
-		boolean three = true;
-		boolean four = true;
+	public boolean checkBox(int[][] board, int x, int y, int value) {
 		for(int i = x; i < x+2; i++) {
 			for(int j = y; j < y+2; j++) {
-				if(board[i][j] == 0)
-					continue;
-				else if(board[i][j] == 1 && one)
-					one = false;
-				else if(board[i][j] == 2 && two)
-					two = false;
-				else if(board[i][j] == 3 && three)
-					three = false;
-				else if(board[i][j] == 4 && four)
-					four = false;
-				else
+				if(board[i][j] == value)
 					return false;
 			}
 		}
 		return true;
 	}
-	public boolean checkSquares(int[][] board) {
-		if(checkBox(board, 0,0) && checkBox(board, 0,2) && checkBox(board,2,0) && checkBox(board,2,2))
+	public boolean checkSquares(int[][] board, int row, int column, int value) {
+		if(row < 2 && column < 2) {
+			if(checkBox(board, 0, 0, value))
 				return true;
-		else
-			return false;
+		}
+		else if(row < 2 && column >= 2) {
+			if(checkBox(board, 0, 2, value))
+				return true;
+		}
+		else if(row >= 2 && column < 2) {
+			if(checkBox(board, 2, 0, value))
+				return true;
+		}
+		else if(row >= 2 && column >= 2) {
+			if(checkBox(board, 2, 2, value))
+				return true;
+		}
+		return false;
 	}
 
 
